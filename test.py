e406fa30a2db45d347a69d725dc5930a0b3bad87
Ethan SMADJA <155528040+ethansmadjaa@users.noreply.github.com>
2025-12-02 01:27:57 +0100

    Replace recipe fetching and caching logic with model training and saving functionality in test.py

--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-from utils.recipes_loader import fetch_all_recipes
-import pandas as pd
-from utils.logger import logger
-import os
+from utils.recsys_scheduler import train_and_save_model
 
-cache_path = "data/recipes_cache.json"
-if os.path.exists(cache_path):
-    try:
-        already_cached_df = pd.read_json(cache_path)
-        logger.info(f"Already cached df size: {len(already_cached_df)}")
-    except ValueError as e:
-        logger.warning(f"Cache file corrupted, will regenerate: {e}")
-        already_cached_df = pd.DataFrame()
-else:
-    logger.info("No cache file found, starting fresh")
-    already_cached_df = pd.DataFrame()
-df = fetch_all_recipes()
-df.info()
+train_and_save_model()
